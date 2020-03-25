7cb2cd76b4bd872f78d805772b359da17f2e3b50
Nicholas Muggio <nicholasmuggio@gmail.com>
2020-03-24 19:06:52 -0600

    Avoid over-sizing UI when in browser

--- MainLevel.gd
+++ MainLevel.gd
@@ -1,16 +1,17 @@
 extends Node2D
 
-onready var ship = $UiControl/VBoxContainer/ViewportContainer/Viewport/Spatial/CSGMesh
+onready var ship = $UiControl/VBoxContainer/ViewportContainer/Viewport/Spatial/Satellite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$UiControl.set_size(get_viewport_rect().size)
+#	$UiControl.set_size(get_viewport_rect().size)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var sliderVal = $UiControl/VBoxContainer/HBoxContainer/GridContainer/HSlider.value
-	$UiControl/VBoxContainer/ViewportContainer.stretch_shrink = sliderVal;
+	$UiControl/VBoxContainer/ViewportContainer.stretch_shrink = sliderVal
 	pass
 
 
